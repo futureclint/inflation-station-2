e08c2865d5b24d7a1737afc6183e789fba1689e2
Clint Gunter <clint@clint.io>
2023-02-19 13:45:18 -0600

    Set up basic page structure and starter styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './css/App.css';
 import './css/styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Inflation Station</h1>
+        <h2>An Inflation Calculator</h2>
       </header>
+      <hr />
+      <main>
+
+      </main>
+      <hr />
+      <footer>
+        <span>&copy; 2021&ndash;Present Clint Gunter</span>
+      </footer>
     </div>
   );
 }
