2b6cc1856503023e5ded17a904b86151b9642bc7
Clint Gunter <clint@clint.io>
2023-02-24 18:21:47 -0600

    Added calculations list that updates on form submit

--- src/components/Calculate.jsx
+++ src/components/Calculate.jsx
@@ -2,51 +2,60 @@ import { useState } from 'react';
 
 function Calculate() {
 
-  const [initialYear, setInitialYear] = useState(null);
-  const [initialAmount, setInitialAmount] = useState(null);
-  const [finalYear, setFinalYear] = useState(null);
+  const [initialYear, setInitialYear] = useState('');
+  const [initialAmount, setInitialAmount] = useState('');
+  const [finalYear, setFinalYear] = useState('');
+  const [calculations, setCalculations] = useState([]);
 
   // Handle form submission
-  const submit = event => {
+  const submit = (event) => {
     // Prevent browser from submitting POST request
     event.preventDefault();
 
+    // Update list of calculations
+    setCalculations([
+      ...calculations,
+      {
+        initialYear: initialYear,
+        initialAmount: initialAmount,
+        finalYear: finalYear
+      }
+    ])
+
     // Clear states back to null
-    setInitialYear(null);
-    setInitialAmount(null);
-    setFinalYear(null);
+    setInitialYear(0);
+    setInitialAmount(0);
+    setFinalYear(0);
   };
 
   return (
-    <div className="calculate-form">
-      <form onSubmit={submit}>
-        <input
-          value={initialYear}
-          onChange={event => setInitialYear(event.target.value)}
-          type="text"
-          placeholder="Initial Year"
-          required
-        />
-        <input
-          value={initialAmount}
-          onChange={event => setInitialAmount(event.target.value)}
-          type="text"
-          placeholder="Initial Amount"
-          required
-        />
-        <br />
-        <input
-          value={finalYear}
-          onChange={event => setFinalYear(event.target.value)}
-          type="text"
-          placeholder="Final Year"
-          required
-        />
-        <input type="text" placeholder="Final Amount" disabled />
-        <br />
-        <button>Calculate</button>
-      </form>
-    </div>
+    <form onSubmit={submit}>
+      <input
+        value={initialYear}
+        onChange={event => setInitialYear(event.target.value)}
+        type="text"
+        placeholder="Initial Year"
+        required
+      />
+      <input
+        value={initialAmount}
+        onChange={event => setInitialAmount(event.target.value)}
+        type="text"
+        placeholder="Initial Amount"
+        required
+      />
+      <br />
+      <input
+        value={finalYear}
+        onChange={event => setFinalYear(event.target.value)}
+        type="text"
+        placeholder="Final Year"
+        required
+      />
+      <input type="text" placeholder="Final Amount" disabled />
+      <br />
+      <button>Calculate</button>
+    </form>
   );
 }
 
