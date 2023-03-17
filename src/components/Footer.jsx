66df1e3565b2d9ede0a9789ff53aa6dd33b7d7de
Clint Gunter <clint@clint.io>
2023-03-17 15:56:59 -0500

    Added layout to header and footer, including a slide-in message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 function Footer() {
   return (
   <footer>
+    <span>View project on <a href="https://github.com/futureclint/inflation-station-2">GitHub</a></span>
+    <span>CPI data from <a href="https://www.bls.gov/cpi/data.htm">the U.S. Bureau of Labor Statistics</a></span>
     <span>&copy; 2021&ndash;Present Clint Gunter</span>
   </footer>
 );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import logo from '../images/inflation-station-logo_dark.svg';
+import cross from '../images/cross.svg';
 
 function Header() {
+
+  const [messageOut, setMessageOut] = useState(false);
+
+  const closeMessage = () => {
+    setMessageOut(true);
+  }
+
   return (
     <header>
-      <img className="logo" src={logo} alt="Inflation Station" draggable="false" />
-      <h2>An Inflation Calculator</h2>
+      <div className={messageOut ? 'message out' : 'message'}>
+        <span><em>This project is in active development, but I'm happy for you to try it out in the meantime!</em></span>
+        <button onClick={closeMessage}>
+          <img className="cross" src={cross} alt="Close" draggable="false" />
+        </button>
+      </div>
+      <div className="title">
+        <img className="logo" src={logo} alt="Inflation Station" draggable="false" />
+        <h2 className="subtitle">An Inflation Calculator</h2>
+      </div>
     </header>
   );
 }
