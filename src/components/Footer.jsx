dfb583d60f28bb824a93d3e47525af478960a950
Clint Gunter <clint@clint.io>
2023-03-23 15:32:44 -0500

    Added better layout to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 function Footer() {
   return (
   <footer>
-    <span>View project on <a href="https://github.com/futureclint/inflation-station-2">GitHub</a></span>
-    <span>CPI data from <a href="https://www.bls.gov/cpi/data.htm">the U.S. Bureau of Labor Statistics</a></span>
-    <span>&copy; 2021&ndash;Present Clint Gunter</span>
+    <section>
+      <span>&copy; 2021&ndash;Present <a href="https://clint.io">Clint Gunter</a></span>
+    </section>
+    <section>
+      <span>View project on <a href="https://github.com/futureclint/inflation-station-2">GitHub</a></span>
+      <span>CPI data from <a href="https://www.bls.gov/cpi/data.htm">the U.S. Bureau of Labor Statistics</a></span>
+    </section>
   </footer>
 );
 }
