f24bb9bfe22bdb1d2177c362fbb149a47df808e2
Clint Gunter <clint@clint.io>
2023-03-31 16:06:21 -0500

    Added close button to calculation list items

--- src/components/Calculations.jsx
+++ src/components/Calculations.jsx
@@ -1,4 +1,5 @@
 import '../css/main.css';
+import cross from '../images/cross.svg';
 
 function Calculations({calculations}) {
   return (
@@ -6,7 +7,12 @@ function Calculations({calculations}) {
       { calculations.length > 0 ?
       <ul>
         {calculations.map((calculation, index) => (
-          <li key={calculation.key}><strong>${calculation.initialAmount}</strong> in <strong>{calculation.initialYear}</strong> is the same as <strong>${calculation.finalAmount}</strong> in <strong>{calculation.finalYear}</strong></li>
+          <li key={calculation.key}>
+            <span><strong>${calculation.initialAmount}</strong> in <strong>{calculation.initialYear}</strong> is the same as <strong>${calculation.finalAmount}</strong> in <strong>{calculation.finalYear}</strong></span>
+            <button className="delete-calculation">
+              <img className="cross" src={cross} alt="Close" draggable="false" />
+            </button>
+          </li>
         ))}
       </ul>
       : <span className="no-calculations"><em>There are no calculations (yet)</em></span> }
